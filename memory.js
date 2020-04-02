91c2332d58235a09ae540336659a522f4b55d6cf
Dante Tokay <valley4stanlley@gmail.com>
2020-04-02 10:59:18 +0200

    initializing repository

---
+++ memory.js
@@ -0,0 +1,20 @@
+var card = ['ciri.png', 'geralt.png', 'jaskier.png', 'iorweth.png', 'tris.png', 'yen.png'];
+card.sort();
+alert(card[0]);
+
+//var c2 = document.getElementById('c2');
+//var c3 = document.getElementById( 'c3');
+$('#c0').addEventListener('click', function(){revealCard(0);});
+
+function revealCard( x ){
+
+   alert(x);
+}
+
+//c2.addEventListener("click", function() { revealCard(2); }) ;
+
+
+
+
+
+
